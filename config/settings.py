62e620fc8e29a87e58db096fa27b9c316a5d2955
Pungushe <y.borozna81@internet.ru>
2023-12-06 20:58:35 +0300

    Добавление медиа

--- config/settings.py
+++ config/settings.py
@@ -106,11 +106,13 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media files
+MEDIA_ROOT=BASE_DIR / 'media'
+MEDIA_URL='/media/'
--- shop/models.py
+++ shop/models.py
@@ -4,6 +4,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название')
     price = models.IntegerField(verbose_name='Цена')
     description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(blank=True, upload_to='images', verbose_name='Изображение')
 
     def __str__(self):
         return self.name
